d72f020ff83b3f1dec8ce8efd9144492512e970a
Food <creaj2022@gmail.com>
2022-07-22 16:32:53 -0600

    correcion
    correccion de mensaje

--- email.php
+++ email.php
@@ -1,10 +1,39 @@
 <?php
-$destinatario ='creaj2022@gmail.com';
+$destinatario ='foodkingdomcreaj@gmail.com';
 //esto es para quien es el correo
 $email= $_POST['email'];
-$name= $_POST['name'];
+$name=$_POST['name'];
+$mensaje;
 
-mail($email, "Bienvenidooo ",$name, " a un nuevo mundo de la comida", "FOOD kingdom");
+$mensaje="Bienvenido a un nuevo mundo de la comida";
+// título
+$título = 'FOOD KINDOM';
+
+// mensaje
+$mensaje = '
+<html>
+<head>
+  <title>Bienvenido</title>
+</head>
+<body>
+  <p>¡Bienvenido a un nuevo reino de la comida!</p>
+  <img src="../images/foto.png">
+</body>
+</html>
+';
+
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+/* Cabeceras adicionales
+$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
+$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
+$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";*/
+
+// Enviarlo
+mail($email, $título, $mensaje, $cabeceras);
 
 include 'register.php';
 ?>
