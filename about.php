64ecf9c897cedcba55f7849fcb00cf7371e0b7fd
CELS09 <107452708+Foodcdb@users.noreply.github.com>
2022-08-18 11:03:39 -0600

    zasdasdas
    asdasdasdas

--- about.php
+++ about.php
@@ -21,7 +21,7 @@ if(!isset($user_id)){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>about</title>
+   <title>Acerca</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
