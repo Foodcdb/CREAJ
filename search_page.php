65f7e10703e389308e179980e7d2d184f886a798
Norman <creaj2022@gmail.com>
2022-08-25 13:45:26 -0600

    NO SE
    AAAAAAAAAA

--- search_page.php
+++ search_page.php
@@ -133,22 +133,16 @@ if(isset($_POST['add_to_cart'])){
       <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
       <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
       <input type="number" min="1" value="1" name="p_qty" class="qty">
-<<<<<<< HEAD
       <input type="submit" value="Agregar a lista de deseos" class="option-btn" name="add_to_wishlist">
       <input type="submit" value="Agregar al carrito" class="btn" name="add_to_cart">
-=======
-      <input type="submit" value="añadir a la lista de deseos" class="option-btn" name="add_to_wishlist">
-      <input type="submit" value="añadir al carrito" class="btn" name="add_to_cart">
->>>>>>> 38cd5d4c421a806f89147876bcc0c581ddc6a752
    </form>
    <?php
          }
       }else{
-<<<<<<< HEAD
+
          echo '<p class="empty">Sin resultados!</p>';
-=======
+
          echo '<p class="empty">no hay resultados!</p>';
->>>>>>> 38cd5d4c421a806f89147876bcc0c581ddc6a752
       }
       
    };
