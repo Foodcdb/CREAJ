86b70ad3397f67ff5a989e51737bfb0d263f25f1
ChupaPijas3000 <creaj2022@gmail.com>
2022-08-25 09:53:45 -0600

    dasddasdsaasd
    asdasdasd

--- cart.php
+++ cart.php
@@ -40,7 +40,7 @@ if(isset($_POST['update_qty'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Carrito de compra</title>
+   <title>Carrito de putas</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
